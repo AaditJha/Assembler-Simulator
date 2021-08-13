6725930627f5c13d4c302281c0c3a859aeb1c768
lot1aus <g7.aadit@gmail.com>
2021-08-13 21:55:12 +0530

    created main.py, needs error-handling

---
+++ Simple-Assembler/main.py
@@ -0,0 +1,142 @@
+import sys
+from collections import OrderedDict
+#regList -> Mapping between opcode in decimal and Type (A as 0,B as 1,...)
+
+memAddDict = OrderedDict()
+
+regList = {
+    "add"  :0,
+    "sub"  :0,
+    "movI" :1,
+    "movR" :2,
+    "ld"   :3,
+    "st"   :3,
+    "mul"  :0,
+    "div"  :2,
+    "rs"   :1,
+    "ls"   :1,
+    "xor"  :0,
+    "or"   :0,
+    "and"  :0,
+    "not"  :2,
+    "cmp"  :2,
+    "jmp"  :4,
+    "jlt"  :4,
+    "jgt"  :4,
+    "je"   :4,
+    "hlt"  :5
+    }
+
+
+binEncoding = [['u2','r','r','r'],['r','im'],
+['u5','r','r'],['r','mem'],['u3','mem'],
+['u11']]
+
+def givBin(intVal,bitsize):
+    intVal = int(intVal)
+    binStr = str(bin(intVal))[2:]
+    binStr = '0'*(bitsize - len(binStr)) + binStr
+    return binStr
+
+def genBin(typ, cmnd):
+    opDec = list(regList.keys()).index(cmnd[0])
+    opBin  = givBin(opDec,5)
+    binOut = opBin
+    instISA = binEncoding[typ]
+    idx = 1
+    for inst in instISA:
+        if inst == 'r':
+            if(cmnd[idx] == "FLAGS"):
+                rBin = "111"
+            else:
+                rBin = givBin(cmnd[idx][-1],3)
+            binOut += rBin
+
+        elif inst == 'mem':
+            addBin = givBin(memAddDict[cmnd[idx]],8)
+            binOut += addBin
+
+        elif inst == 'im':
+            imBin = givBin(cmnd[idx][1:],8)
+            binOut += imBin
+
+        elif inst[0] == 'u':
+            idx -= 1
+            bitSize = int(inst[1:])
+            binOut += '0'*bitSize
+        else:
+            return
+
+        idx += 1
+    return binOut
+
+def getOpType(cmnd):
+    if cmnd[0] == 'mov':
+        if cmnd[2][0] == "$":
+            cmnd[0] = 'movI'
+            return regList["movI"]
+        cmnd[0] = 'movR'
+        return regList["movR"]
+    return regList[cmnd[0]]
+
+def gotError(cmdLine,lncount):
+    return False
+
+def readCmnd(cmndLine):
+    '''
+    take a string and return it after splitting making a list.
+    '''
+    return cmndLine.split()
+
+def preProcess():
+    '''
+    check for errors
+    label/variable address map
+    return list of strings where each string is a line in asm code.
+    in case of error, call errorHandler it will return -1.
+    '''
+    inputCode = []
+    lncount = 0
+    # for line in sys.stdin:
+    for line in sys.stdin:
+        line = readCmnd(line)
+        if len(line):
+            if gotError(line,lncount):
+                return -1
+            if line[0] == 'var':
+                memAddDict[line[1]] = -1
+                lncount -= 1
+            elif line[0][-1] == ':':
+                memAddDict[line[0][:-1]] = lncount
+                inputCode.append(line[1:])
+            else:
+                inputCode.append(line)
+            lncount += 1
+    
+    varidx = 1
+    for addKey in memAddDict.keys():
+        if memAddDict[addKey] == -1:
+            memAddDict[addKey] = varidx+lncount-1
+            varidx += 1
+    return inputCode
+
+def writeBin(binOut):
+    '''
+    taking a string which is a new line of code and writing it in output file.
+    '''
+    sys.stdout.write(binOut+'\n')
+
+def runAssembler(asmCode):
+    for codeLine in asmCode:
+        typ = getOpType(codeLine)
+        binOut = genBin(typ,codeLine)
+        writeBin(binOut)
+
+def main():
+    asmCode = preProcess()
+    if(asmCode != -1):
+        runAssembler(asmCode)
+
+if __name__ == "__main__":
+    main()
+
